529497411fd0d2278a95f2a04f09ac8b3a045064
arju10 <arjuarju71@gmail.com>
2025-07-09 09:53:48 +0600

    Update3

--- bangla_agriculture_chatbot/settings.py
+++ bangla_agriculture_chatbot/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
